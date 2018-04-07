db27c2a8d3adc1000aad86e7051c513943c904e1
Matt Schuchard <grapheneowns@gmail.com>
2018-04-07 09:58:33 -0400

    begin settings refactor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 1.6.0 (Next)
+- Minimum Ruby version increased to 2.1.
+- Minimum Puppet version increased to 3.7.
+
 ### 1.5.1
 - Slight cleanup and optimization.
 - Fixed check for no spec directories during RSpec Puppet helper.
--- lib/puppet-check/cli.rb
+++ lib/puppet-check/cli.rb
@@ -18,6 +18,37 @@ class PuppetCheck::CLI
     # show help message if no args specified
     args = %w[-h] if args.empty?
 
+    # initialize settings hash
+    settings = {}
+
+    # initialize future parser, fail on warning,  style check, and regression check bools
+    settings[:future_parser] = false
+    settings[:fail_on_warning] = false
+    settings[:style_check] = false
+    settings[:smoke_check] = false
+    settings[:regression_check] = false
+
+    # initialize ssl keys for eyaml checks
+    settings[:public] = nil
+    settings[:private] = nil
+
+    # initialize output format option
+    settings[:output_format] = 'text'
+
+    # initialize octocatalog-diff options
+    settings[:octoconfig] = '.octocatalog-diff.cfg.rb'
+    settings[:octonodes] = %w[localhost.localdomain]
+
+    # initialize diagnostic output arrays
+    settings[:error_files] = []
+    settings[:warning_files] = []
+    settings[:clean_files] = []
+    settings[:ignored_files] = []
+
+    # initialize style arg arrays
+    settings[:puppetlint_args] = []
+    settings[:rubocop_args] = []
+
     opt_parser = OptionParser.new do |opts|
       # usage
       opts.banner = 'usage: puppet-check [options] paths'
@@ -29,33 +60,34 @@ class PuppetCheck::CLI
       end
 
       # bool options
-      opts.on('-f', '--future', 'Enable future parser') { PuppetCheck.settings[:future_parser] = true }
-      opts.on('--fail-on-warnings', 'Fail on warnings') { PuppetCheck.settings[:fail_on_warnings] = true }
-      opts.on('-s', '--style', 'Enable style checks') { PuppetCheck.settings[:style_check] = true }
-      opts.on('--smoke', 'Enable smoke testing') { PuppetCheck.settings[:smoke_check] = true }
-      opts.on('-r', '--regression', 'Enable regression testing (in progress, do not use)') { PuppetCheck.settings[:regression_check] = true }
+      opts.on('-f', '--future', 'Enable future parser') { settings[:future_parser] = true }
+      opts.on('--fail-on-warnings', 'Fail on warnings') { settings[:fail_on_warnings] = true }
+      opts.on('-s', '--style', 'Enable style checks') { settings[:style_check] = true }
+      opts.on('--smoke', 'Enable smoke testing') { settings[:smoke_check] = true }
+      opts.on('-r', '--regression', 'Enable regression testing (in progress, do not use)') { settings[:regression_check] = true }
 
       # ssl key options for eyaml checks
-      opts.on('--public cert.pem', String, 'Public key for EYAML checks') { |arg| PuppetCheck.settings[:public] = arg }
-      opts.on('--private cert.pem', String, 'Private key for EYAML checks') { |arg| PuppetCheck.settings[:private] = arg }
+      opts.on('--public cert.pem', String, 'Public key for EYAML checks') { |arg| settings[:public] = arg }
+      opts.on('--private cert.pem', String, 'Private key for EYAML checks') { |arg| settings[:private] = arg }
 
       # formatting options
-      opts.on('-o', '--output format', String, 'Format for results output (default is text): text, json, or yaml') { |arg| PuppetCheck.settings[:output_format] = arg }
+      opts.on('-o', '--output format', String, 'Format for results output (default is text): text, json, or yaml') { |arg| settings[:output_format] = arg }
 
       # octocatalog-diff options
-      opts.on('--octoconfig config_file', String, 'Octocatalog-diff configuration file to use') { |arg| PuppetCheck.settings[:octoconfig] = arg }
-      opts.on('-n', '--octonodes node1.example.com,node2.example.com', Array, 'Octocatalog-diff nodes to test catalog on') { |arg| PuppetCheck.settings[:octonodes] = arg }
+      opts.on('--octoconfig config_file', String, 'Octocatalog-diff configuration file to use') { |arg| settings[:octoconfig] = arg }
+      opts.on('-n', '--octonodes node1.example.com,node2.example.com', Array, 'Octocatalog-diff nodes to test catalog on') { |arg| settings[:octonodes] = arg }
 
       # arguments to style checkers
       opts.on('--puppet-lint arg_one,arg_two', Array, 'Arguments for PuppetLint ignored checks') do |puppetlint_args|
-        PuppetCheck.settings[:puppetlint_args].concat(puppetlint_args.map { |arg| "--#{arg}" })
+        settings[:puppetlint_args].concat(puppetlint_args.map { |arg| "--#{arg}" })
       end
       opts.on('-c', '--config file', String, 'Load PuppetLint options from file') do |file|
-        PuppetCheck.settings[:puppetlint_args].concat(File.read(file).split("\n"))
+        settings[:puppetlint_args].concat(File.read(file).split("\n"))
       end
-      opts.on('--rubocop arg_one,arg_two', String, 'Arguments for Rubocop disabled cops') { |arg| PuppetCheck.settings[:rubocop_args] = ['--except', arg] }
+      opts.on('--rubocop arg_one,arg_two', String, 'Arguments for Rubocop disabled cops') { |arg| settings[:rubocop_args] = ['--except', arg] }
     end
 
+    PuppetCheck.settings = settings
     opt_parser.parse!(args)
   end
 end
