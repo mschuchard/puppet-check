17edd5e726774c338b924700e1f88db6e6c82de1
Matt Schuchard <grapheneowns@gmail.com>
2022-02-07 14:02:43 -0500

    update rspec global var establish

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,13 @@ require 'rspec'
 module Variables
   extend RSpec::SharedContext
 
-  let(:fixtures_dir) { "#{File.dirname(__FILE__)}/fixtures/" }
-  let(:octocatalog_diff_dir) { "#{File.dirname(__FILE__)}/octocatalog-diff/" }
+  def fixtures_dir
+    @fixtures_dir = "#{File.dirname(__FILE__)}/fixtures/"
+  end
+
+  def octocatalog_diff_dir
+    @octocatalog_diff_dir = "#{File.dirname(__FILE__)}/octocatalog-diff/"
+  end
 end
 
 RSpec.configure do |config|
