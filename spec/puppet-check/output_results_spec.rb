500ccfbf70aa97c9507e2d5357cefb93803c8f8c
Matt Schuchard <grapheneowns@gmail.com>
2022-12-02 11:02:23 -0500

    update output and data unit tests

--- spec/puppet-check/output_results_spec.rb
+++ spec/puppet-check/output_results_spec.rb
@@ -4,14 +4,14 @@ require_relative '../../lib/puppet-check/output_results'
 describe OutputResults do
   context '.text' do
     before(:each) do
-      PuppetCheck.settings[:error_files] = []
+      PuppetCheck.settings[:error_files] = {}
       PuppetCheck.settings[:warning_files] = []
       PuppetCheck.settings[:clean_files] = {}
       PuppetCheck.settings[:ignored_files] = {}
     end
 
     it 'outputs files with errors' do
-      PuppetCheck.settings[:error_files] = ['foo: i had an error']
+      PuppetCheck.settings[:error_files] = { 'foo' => 'i had an error' }
       expect { OutputResults.text }.to output("\033[31mThe following files have errors:\033[0m\n-- foo: i had an error\n").to_stdout
     end
     it 'outputs files with warnings' do
@@ -30,15 +30,15 @@ describe OutputResults do
 
   context '.markup' do
     before(:each) do
-      PuppetCheck.settings[:error_files] = []
+      PuppetCheck.settings[:error_files] = {}
       PuppetCheck.settings[:warning_files] = []
       PuppetCheck.settings[:clean_files] = {}
       PuppetCheck.settings[:ignored_files] = {}
     end
 
     it 'outputs files with errors as yaml' do
-      PuppetCheck.settings[:error_files] = ['foo: i had an error']
-      expect { OutputResults.markup('yaml') }.to output("---\nerrors:\n- 'foo: i had an error'\n").to_stdout
+      PuppetCheck.settings[:error_files] = { 'foo' => 'i had an error' }
+      expect { OutputResults.markup('yaml') }.to output("---\nerrors:\n  foo: i had an error\n").to_stdout
     end
     it 'outputs files with warnings as yaml' do
       PuppetCheck.settings[:warning_files] = ['foo: i had a warning']
@@ -53,8 +53,8 @@ describe OutputResults do
       expect { OutputResults.markup('yaml') }.to output("---\nignored:\n- foo\n").to_stdout
     end
     it 'outputs files with errors as json' do
-      PuppetCheck.settings[:error_files] = ['foo: i had an error']
-      expect { OutputResults.markup('json') }.to output("{\n  \"errors\": [\n    \"foo: i had an error\"\n  ]\n}\n").to_stdout
+      PuppetCheck.settings[:error_files] = { 'foo' => 'i had an error' }
+      expect { OutputResults.markup('json') }.to output("{\n  \"errors\": {\n    \"foo\": \"i had an error\"\n  }\n}\n").to_stdout
     end
     it 'outputs files with warnings as json' do
       PuppetCheck.settings[:warning_files] = ['foo: i had a warning']
