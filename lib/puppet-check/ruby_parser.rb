5aa3892c2392606043b588d69098590db457d01c
Matt Schuchard <grapheneowns@gmail.com>
2022-12-12 08:47:19 -0500

    remove superfluous ruby file error info

--- lib/puppet-check/ruby_parser.rb
+++ lib/puppet-check/ruby_parser.rb
@@ -10,7 +10,7 @@ class RubyParser
       # prevents ruby code from actually executing
       catch(:good) { instance_eval("BEGIN {throw :good}; #{File.read(file)}", file) }
     rescue ScriptError, StandardError => err
-      PuppetCheck.settings[:error_files][file] = err.to_s
+      PuppetCheck.settings[:error_files][file] = err.to_s.gsub("#{file}:", '')
     else
       # check ruby style
       if style
@@ -48,7 +48,7 @@ class RubyParser
         # empty out warnings since it would contain an error if this pass triggers
         warnings = ''
       rescue ScriptError => err
-        next PuppetCheck.settings[:error_files][file] = err.to_s
+        next PuppetCheck.settings[:error_files][file] = err.to_s.gsub('(erb):', '')
       end
       # return warnings from the check if there were any
       next PuppetCheck.settings[:warning_files].push("#{file}:\n#{warnings.gsub('warning: ', '').split('(erb):').join.strip}") unless warnings == ''
@@ -71,7 +71,7 @@ class RubyParser
       # prevents ruby code from actually executing
       catch(:good) { instance_eval("BEGIN {throw :good}; #{File.read(file)}", file) }
     rescue SyntaxError, LoadError, ArgumentError => err
-      PuppetCheck.settings[:error_files][file] = err.to_s
+      PuppetCheck.settings[:error_files][file] = err.to_s.gsub("#{file}:", '')
     # check librarian puppet style
     else
       if style
