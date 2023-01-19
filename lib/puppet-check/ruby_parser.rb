4aea059aeadc0098bf42cb4a2dd10864e9939a72
Matt Schuchard <grapheneowns@gmail.com>
2023-01-19 10:40:25 -0500

    line and col info for ruby warnings

--- lib/puppet-check/ruby_parser.rb
+++ lib/puppet-check/ruby_parser.rb
@@ -19,17 +19,17 @@ class RubyParser
         require 'rubocop'
 
         rubocop_warnings = Utils.capture_stdout { RuboCop::CLI.new.run(rc_args + ['--enable-pending-cops', '--require', 'rubocop-performance', '--format', 'json', file]) }
-        rubocop_offenses = JSON.parse(rubocop_warnings)['files'][0]['offenses']
+        rubocop_offenses = JSON.parse(rubocop_warnings)['files'][0]['offenses'].map { |warning| "#{warning['location']['line']}:#{warning['location']['column']} #{warning['message']}" }
 
         # check Reek
         require 'reek'
         require 'reek/cli/application'
 
         reek_warnings = Utils.capture_stdout { Reek::CLI::Application.new(['-f', 'json', file]).execute }
-        reek_offenses = JSON.parse(reek_warnings).map { |warning| "#{warning['context']} #{warning['message']}" }
+        reek_offenses = JSON.parse(reek_warnings).map { |warning| "#{warning['lines']}: #{warning['context']} #{warning['message']}" }
 
         # assign warnings from combined offenses
-        warnings = rubocop_offenses.map { |offense| offense['message'] } + reek_offenses
+        warnings = rubocop_offenses + reek_offenses
 
         # return warnings
         next PuppetCheck.settings[:warning_files][file] = warnings unless warnings.empty?
