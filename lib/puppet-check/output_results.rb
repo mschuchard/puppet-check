99cc9f93056f6224837722c40eafb11ce0eb03f6
Matt Schuchard <grapheneowns@gmail.com>
2023-06-06 15:15:03 -0400

    improve output results text format newlines

--- lib/puppet-check/output_results.rb
+++ lib/puppet-check/output_results.rb
@@ -31,15 +31,15 @@ class OutputResults
     # errors
     category_info("\033[31mThe following files have errors:\033[0m\n", files[:errors]) if files.key?(:errors)
     # warnings
-    category_info("\n\033[33mThe following files have warnings:\033[0m\n", files[:warnings]) if files.key?(:warnings)
+    category_info("\033[33mThe following files have warnings:\033[0m\n", files[:warnings]) if files.key?(:warnings)
     # cleans
-    category_info("\n\033[32mThe following files have no errors or warnings:\033[0m\n-- ", files[:clean]) if files.key?(:clean)
+    category_info("\033[32mThe following files have no errors or warnings:\033[0m\n-- ", files[:clean]) if files.key?(:clean)
     # ignores
     return unless files.key?(:ignored)
-    category_info("\n\033[36mThe following files have unrecognized formats and therefore were not processed:\033[0m\n-- ", files[:ignored])
+    category_info("\033[36mThe following files have unrecognized formats and therefore were not processed:\033[0m\n-- ", files[:ignored])
   end
 
-  # display heading, files, and file messages per category
+  # display heading, files, and file messages per category for text formatting
   def self.category_info(heading, files)
     private_class_method :method
     # display category heading
@@ -51,5 +51,8 @@ class OutputResults
     when Array then puts files.join("\n-- ")
     else raise "puppet-check: The category files were of unexpected type #{files.class}. Please file an issue with this log message, category heading, and information about the parsed files."
     end
+
+    # newline between categories for easier visual parsing
+    puts ''
   end
 end
