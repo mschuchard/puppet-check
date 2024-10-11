8345b29e7926e7c6994a1379a8f3d60ee9e90d49
Matt Schuchard <grapheneowns@gmail.com>
2024-10-11 12:58:07 -0400

    prototype refactor text output

--- lib/puppet-check/output_results.rb
+++ lib/puppet-check/output_results.rb
@@ -2,6 +2,13 @@ require_relative '../puppet_check'
 
 # class to handle outputting diagnostic results in desired format
 class OutputResults
+  HEADER = {
+    errors: "\033[31mThe following files have errors:\033[0m\n",
+    warnings: "\033[33mThe following files have warnings:\033[0m\n",
+    clean: "\033[32mThe following files have no errors or warnings:\033[0m\n-- ",
+    ignored: "\033[36mThe following files have unrecognized formats and therefore were not processed:\033[0m\n-- "
+  }.freeze
+
   # output the results in various formats
   def self.run(files, format)
     # remove empty entries
@@ -28,31 +35,26 @@ class OutputResults
   def self.text(files)
     private_class_method :method
 
-    # errors
-    category_info("\033[31mThe following files have errors:\033[0m\n", files[:errors]) if files.key?(:errors)
-    # warnings
-    category_info("\033[33mThe following files have warnings:\033[0m\n", files[:warnings]) if files.key?(:warnings)
-    # cleans
-    category_info("\033[32mThe following files have no errors or warnings:\033[0m\n-- ", files[:clean]) if files.key?(:clean)
-    # ignores
-    return unless files.key?(:ignored)
-    category_info("\033[36mThe following files have unrecognized formats and therefore were not processed:\033[0m\n-- ", files[:ignored])
-  end
+    # output text for each of four file categories
+    %i[errors warnings clean ignored].each do |category|
+      # immediately return if category is empty
+      next unless files.key?(category)
 
-  # display heading, files, and file messages per category for text formatting
-  def self.category_info(heading, files)
-    private_class_method :method
-    # display category heading
-    print heading
+      # display category heading
+      print HEADER[category]
 
-    # display files and optionally messages
-    case files
-    when Hash then files.each { |file, messages| puts "-- #{file}:\n#{messages.join("\n")}" }
-    when Array then puts files.join("\n-- ")
-    else raise "puppet-check: The category files were of unexpected type #{files.class}. Please file an issue with this log message, category heading, and information about the parsed files."
+      # display heading, files, and file messages per category for text formatting
+      files = files[category]
+
+      # display files and optionally messages
+      case files
+      when Hash then files.each { |file, messages| puts "-- #{file}:\n#{messages.join("\n")}" }
+      when Array then puts files.join("\n-- ")
+      else raise "puppet-check: The files category was of unexpected type #{files.class}. Please file an issue with this log message, category heading, and information about the parsed files."
+      end
+
+      # newline between categories for easier visual parsing
+      puts ''
     end
-
-    # newline between categories for easier visual parsing
-    puts ''
   end
 end
