69be75a9214878f10200bec4615fb96d9266ca4c
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-07-28 10:23:44 -0400

    octo-diff dep rugged fails; fix regression class for no octo-diff

--- lib/puppet-check/regression_check.rb
+++ lib/puppet-check/regression_check.rb
@@ -1,29 +1,33 @@
-# temporarily supress warning messages for octocatalog-diff redefining puppet constants and then reactivate
-$VERBOSE = nil
-require 'octocatalog-diff'
-$VERBOSE = false
+begin
+  # temporarily supress warning messages for octocatalog-diff redefining puppet constants and then reactivate
+  $VERBOSE = nil
+  require 'octocatalog-diff'
+  $VERBOSE = false
 
-# executes smoke and regression tests on catalogs
-class RegressionCheck
-  # smoke testing
-  def self.smoke(interface_nodes, octoconfig)
-    options = config(octoconfig)
-    nodes = options.key?(:node) ? [options[:node]] : interface_nodes
-    nodes.each do |node|
-      options[:node] = node
-      OctocatalogDiff::API::V1.catalog(options)
+  # executes smoke and regression tests on catalogs
+  class RegressionCheck
+    # smoke testing
+    def self.smoke(interface_nodes, octoconfig)
+      options = config(octoconfig)
+      nodes = options.key?(:node) ? [options[:node]] : interface_nodes
+      nodes.each do |node|
+        options[:node] = node
+        OctocatalogDiff::API::V1.catalog(options)
+      end
+    end
+
+    # regression testing
+    # def self.regression(nodes, octoconfig)
+    #   options = RegressionCheck.config(octoconfig)
+    #   nodes.each { |node| stuff }
+    # end
+
+    # config file loading
+    def self.config(octoconfig)
+      private_class_method :method
+      OctocatalogDiff::API::V1.config(filename: octoconfig)
     end
   end
-
-  # regression testing
-  # def self.regression(nodes, octoconfig)
-  #   options = RegressionCheck.config(octoconfig)
-  #   nodes.each { |node| stuff }
-  # end
-
-  # config file loading
-  def self.config(octoconfig)
-    private_class_method :method
-    OctocatalogDiff::API::V1.config(filename: octoconfig)
-  end
+rescue LoadError
+  # octocatalog-diff not installed
 end
