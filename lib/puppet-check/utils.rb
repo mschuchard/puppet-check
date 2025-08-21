e473ec490be515d0ba9553c726e410b3aecad195
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-08-21 09:59:48 -0400

    io capture thread safety

--- lib/puppet-check/utils.rb
+++ lib/puppet-check/utils.rb
@@ -1,22 +1,26 @@
+require 'stringio'
+
 # utility methods totally not edited from StackOverflow
 class Utils
   # captures stdout from a block: out = capture_stdout { code }
   def self.capture_stdout
-    old_stdout = $stdout
+    Thread.current[:old_stdout] = $stdout
     $stdout = StringIO.new
     yield
     $stdout.string
   ensure
-    $stdout = old_stdout
+    $stdout = Thread.current[:old_stdout] if Thread.current[:old_stdout]
+    Thread.current[:old_stdout] = nil
   end
 
   # captures stderr from a block: err = capture_stderr { code }
   def self.capture_stderr
-    old_stderr = $stderr
+    Thread.current[:old_stderr] = $stderr
     $stderr = StringIO.new
     yield
     $stderr.string
   ensure
-    $stderr = old_stderr
+    $stderr = Thread.current[:old_stderr] if Thread.current[:old_stderr]
+    Thread.current[:old_stderr] = nil
   end
 end
