583468448d65073b4228c4f365708094639f4efa
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-10-03 12:31:09 -0400

    update system test and lower ruby version max in ci

--- spec/system/system_spec.rb
+++ spec/system/system_spec.rb
@@ -5,24 +5,20 @@ require_relative '../../lib/puppet-check/tasks'
 
 describe PuppetCheck do
   context 'executed as a system from the CLI with arguments and various files to be processed' do
-    # see regression_check_spec
-    if ENV['CIRCLECI'] == 'true' || ENV['GITHUB_ACTIONS'] == 'true'
-      let(:cli) { PuppetCheck::CLI.run(%w[-s --puppet-lint no-hard_tabs-check,no-140chars-check --rubocop Layout/LineLength,Style/Encoding --public keys/public_key.pkcs7.pem --private keys/private_key.pkcs7.pem .]) }
-    else
-      let(:cli) { PuppetCheck::CLI.run(%w[-s --puppet-lint no-hard_tabs-check,no-140chars-check --rubocop Layout/LineLength,Style/Encoding --public keys/public_key.pkcs7.pem --private keys/private_key.pkcs7.pem --smoke -n good.example.com --octoconfig spec/octocatalog-diff/octocatalog-diff.cfg.rb .]) }
-    end
-
     it 'outputs diagnostic results correctly after processing all of the files' do
       Dir.chdir(fixtures_dir)
 
-      expect { cli }.not_to raise_exception
+      # see regression_check_spec
+      if ci_env
+        expect(PuppetCheck::CLI.run(%w[-s --puppet-lint no-hard_tabs-check,no-140chars-check --rubocop Layout/LineLength,Style/Encoding --public keys/public_key.pkcs7.pem --private keys/private_key.pkcs7.pem .])).to eql(2)
+      else
+        expect(PuppetCheck::CLI.run(%w[-s --puppet-lint no-hard_tabs-check,no-140chars-check --rubocop Layout/LineLength,Style/Encoding --public keys/public_key.pkcs7.pem --private keys/private_key.pkcs7.pem --smoke -n good.example.com --octoconfig spec/octocatalog-diff/octocatalog-diff.cfg.rb .])).to eql(2)
+      end
 
       expect(PuppetCheck.files[:errors].length).to eql(11)
-      expect(PuppetCheck.files[:warnings].length).to eql(12)
-      expect(PuppetCheck.files[:clean].length).to eql(14)
+      expect(PuppetCheck.files[:warnings].length).to eql(13)
+      expect(PuppetCheck.files[:clean].length).to eql(13)
       expect(PuppetCheck.files[:ignored].length).to eql(3)
-
-      expect(cli).to eql(2)
     end
   end
 
@@ -64,7 +60,7 @@ describe PuppetCheck do
         # assign settings
         settings = { style: true }
         # see regression_check_spec
-        unless ENV['CIRCLECI'] == 'true' || ENV['GITHUB_ACTIONS'] == 'true'
+        unless ci_env
           settings[:smoke] = true
           settings[:octonodes] = %w[good.example.com]
           settings[:octoconfig] = 'spec/octocatalog-diff/octocatalog-diff.cfg.rb'
